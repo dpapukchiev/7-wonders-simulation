339e0508a8a12313aa4b612cb07abc93cb624978
dpapukchiev <dpapukchiev@gmail.com>
2023-09-17 16:20:44 +0200

    spring

---
+++ src/main/java/dpapukchiev/sevenwonderssimulation/SevenWondersSimulationApplication.java
@@ -0,0 +1,13 @@
+package dpapukchiev.sevenwonderssimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevenWondersSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SevenWondersSimulationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dpapukchiev/sevenwonderssimulation/SevenWondersSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package dpapukchiev.sevenwonderssimulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevenWondersSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
